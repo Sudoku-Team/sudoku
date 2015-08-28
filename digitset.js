a9baec82e08d6980bd4f12430e453d693b91838b
Joe Fraley <jose.fraley@gmail.com>
2015-08-28 11:40:00 -0700

    refines digitSet.update methods to perform several distinct functions

--- digitset.js
+++ digitset.js
@@ -2,12 +2,15 @@ DigitSet = ( function ( ) {
 
   	function digitSet ( digit ) {
   
-   	this.possibilities = [];
+	   	this.possibilities = [];
 		this.possibilities.length = 10;
 
-    
-   for ( var i = 0; i < 10; i++ ) {
+	    for ( var i = 0; i < 10; i++ ) {
 			this.possibilities[i] = false;
+		}
+
+		if ( arguments.length < 1 ) {
+			return;
 		}
 
 		if (digit !== '.' ) { 
@@ -21,9 +24,72 @@ DigitSet = ( function ( ) {
 		}   
 	}
 
-	digitSet.prototype.update = function ( digit, boolean ) {
-		this.possibilities[ digit ] = boolean;
+	digitSet.prototype.set = function ( array ) {
+		
+		//change all the indexes of possibilities
+		//cooresponding to numbers in the array to be possible
+		//array = [0, 1, 2]
+    	//this.possibilities = [false, false, false, false...]
+    	//RETURN this.possibilities => [true, true, true, false...]
+    	//does the key name = any value in array?
+    	// i.e. does array have 0 as a value? then key 0 should be true
+    
+    
+		for (var i = 0; i < 10; i++ ){
+
+			for ( var j = 0; j < array.length; j++ ){
+        		if ( array[j] === i ){
+        			
+        			if ( array[j] === '.' ){
+        				return;
+        			}
+        			
+        			else {
+						this.possibilities[i] = true;	
+					}
+				}
+			}
+		}
+
+		return this.possibilities;
 	}
+
+	digitSet.prototype.array = function () {
+		var array = [];
+
+		for (var i = 0; i < 10; i++){
+
+        	if ( this.possibilities[i] === true ){
+        		array.push( i );
+        	}
+        	else {
+        		array.push( '.' );
+        	}
+		}
+
+		return array;
+	}
+
+	digitSet.prototype.add = function ( digit ) {
+		this.possibilities[ digit ] = true;
+	}
+
+	digitSet.prototype.addSet = function ( digitSet ) {
+		this.set( digitSet.array() )
+	}
+
+	digitSet.prototype.eliminate = function ( digit ) {
+		this.possibilities[ digit ] = false;
+	}
+
+	digitSet.prototype.eliminateSet = function ( digitSet ) {
+		for ( var i = 0; i < digitSet.possibilities.length; i++ ) {
+
+			if ( digitSet.possibilities[i] === true ) {
+				this.eliminate( i );
+			}
+		}
+	}	
 
 	digitSet.prototype.size = function (){
 		//how many numbers are still possible here?
@@ -38,27 +104,7 @@ DigitSet = ( function ( ) {
 		return array.length;
 	}
 
-	digitSet.prototype.set = function ( array ) {
-		
-		//change all the numbers in the array to be possible
-		//array = [0, 1, 2]
-    	//this.possibilities = [false, false, false, false...]
-    	//RETURN this.possibilities => [true, true, true, false...]
-    	//does the key name = any value in array?
-    	// i.e. does array have 0 as a value? then key 0 should be true
-    
-    
-		for (var i = 0; i < 10; i++ ){
-
-			for ( var j = 0; j < array.length; j++ ){
-        if ( array[j] === i ){
-					this.possibilities[i] = true;	
-				}
-			}
-		}
-
-		return this.possibilities;
-	}
+	
 
 
 
@@ -86,21 +132,7 @@ DigitSet = ( function ( ) {
     	}
 	}	
 
-	digitSet.prototype.array = function () {
-		var array = [];
-
-		for (var i = 0; i < 10; i++){
-
-        	if ( this.possibilities[i] === true ){
-        		array.push( i );
-        	}
-        	else {
-        		array.push( '.' );
-        	}
-		}
-
-		return array;
-	}
+	
 
 	digitSet.prototype.string = function () {
 		var array = [];
